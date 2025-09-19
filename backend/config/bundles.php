c8d7d9ecceab17278c11e2c9b477c03051864c8f
tech75rus <tech75rus@mail.ru>
2025-09-19 19:30:08 +0900

    install bundles

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
